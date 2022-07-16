d4d36ef53760c3f046cec634d1c15613fd648b10
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-16 10:32:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Pricing from './components/Pricing.vue';
+</script>
+
+<template>
+  <Pricing />
+</template>
+
+<style>
+:root {
+  --dark-blue: hsl(227, 35%, 25%);
+  --gray-blue: hsl(225, 20%, 60%);
+  --bg-white: hsl(230, 100%, 99%);
+}
+
+#app {
+  background-color: var(--bg-white);
+  font-family: 'Manrope', sans-serif;
+}
+</style>
---
+++ src/components/Pricing.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<template>
+  <header>
+    <h1>Simple trafic-based pricing</h1>
+    <h4>Sign-up for our 30-day trial. No credit card required.</h4>
+  </header>
+</template>
+
+<style>
+header {
+  text-align: center;
+  margin-top: 80px;
+  color: var(--dark-blue);
+  background-color: var(--bg-white);
+}
+
+header h4 {
+  font-size: 15px;
+}
+</style>
