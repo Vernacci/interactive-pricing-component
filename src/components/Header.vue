27ee84ea5bb04adda92c49b037f453f03c73f980
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-16 12:08:20 -0300

    update css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,20 +4,25 @@ import { ref } from 'vue';
 
 <template>
   <header>
-    <h1>Simple trafic-based pricing</h1>
-    <h4>Sign-up for our 30-day trial. No credit card required.</h4>
+    <h1 class="title">Simple trafic-based pricing</h1>
+    <p class="sub-title">
+      Sign-up for our 30-day trial. No credit card required.
+    </p>
   </header>
 </template>
 
 <style>
 header {
   text-align: center;
-  margin-top: 80px;
+  margin-top: 30px;
   color: var(--dark-blue);
   background-color: var(--bg-white);
+  background: url('../images/pattern-circles.svg') no-repeat center;
+  padding: 20px;
 }
 
-header h4 {
-  font-size: 15px;
+.sub-title {
+  font-size: 0.9375rem;
+  color: var(--gray-blue);
 }
 </style>
