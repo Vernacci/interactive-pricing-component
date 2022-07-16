c41cc4a6b2e1e4b201bd24a92da42091696fd092
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-16 10:36:38 -0300

    Added Slider component

---
+++ src/components/Slider.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <main class="container">
+    <div class="content">
+      <h3>100K pageviews</h3>
+      <p class="price">$16.00/month</p>
+      <div class="slider-container">
+        <input type="range" name="uau" id="" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
