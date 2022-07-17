b4c9fc7ff1d62d40d71b21071a17fd87658776f4
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-17 11:44:12 -0300

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from './components/Header.vue';
+import Slider from './components/Slider.vue';
 </script>
 
 <template>
   <Header />
+  <Slider />
 </template>
 
 <style>
