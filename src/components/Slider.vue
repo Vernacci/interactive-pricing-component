4e403a2e7c72acd30dc00fdcf6ec31b648ee591c
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-17 16:33:39 -0300

    css update

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -60,6 +60,8 @@ watch(price, (newPrice) => {
 </template>
 
 <style>
+@import url('../css/toggle.css');
+
 .container {
   display: flex;
   justify-content: center;
@@ -106,73 +108,5 @@ watch(price, (newPrice) => {
 
 .pricing {
   text-align: center;
-}
-
-.toggle-container {
-  margin-top: 80px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 20px;
-}
-
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
-
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-.toggle {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  -webkit-transition: 0.4s;
-  transition: 0.4s;
-  width: 60px;
-}
-
-.toggle:before {
-  position: absolute;
-  content: '';
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  -webkit-transition: 0.4s;
-  transition: 0.4s;
-}
-
-input:checked + .toggle {
-  background-color: #2196f3;
-}
-
-input:focus + .toggle {
-  box-shadow: 0 0 1px #2196f3;
-}
-
-input:checked + .toggle:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
-
-.toggle.round {
-  border-radius: 34px;
-}
-
-.toggle.round:before {
-  border-radius: 50%;
 }
 </style>
